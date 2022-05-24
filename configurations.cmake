958ecf8281aca282440b2c3024f8c7ff709c70ef
gmh5225 <2315157@qq.com>
2022-05-24 14:10:50 +0800

    Update

--- configurations.cmake
+++ configurations.cmake
@@ -4,6 +4,12 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Output)
 # disable shared libs
 set(BUILD_SHARED_LIBS OFF)
 
+# only X86
+set(UNICORN_ARCH "x86")
+
+# enable testing
+set(UNICORN_BUILD_TESTS ON)
+
 # add source directory macro
 add_definitions(-DMYTEST_SRC_DIR="${CMAKE_CURRENT_SOURCE_DIR}")
 
